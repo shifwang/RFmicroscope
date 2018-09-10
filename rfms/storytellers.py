d651bffc83ee231cd247e9700f5aa0bf73e518f8
Yu Wang <yu@opensuse>
2018-09-09 21:22:07 -0700

    crucial fixed storytellers

--- rfms/storytellers.py
+++ rfms/storytellers.py
@@ -1,5 +1,6 @@
 from . import readers
 import numpy as np
+import pandas as pd
 def individual_signed_feature_importance(forestReader):
     '''Compute the feature importance for each sample
     Args:
@@ -10,19 +11,21 @@ def individual_signed_feature_importance(forestReader):
     >>> import RFmicroscope
     >>> #TODO
     '''
-    if forestReader.TreeReaderType != 'ImportanceTreeReader':
-        raise AssertionError('TreeReaderType must be ImportanceTreeReader')
+    if forestReader.TreeReaderType != 'Importance':
+        raise AssertionError('TreeReaderType must be Importance but got %s'%forestReader.TreeReaderType)
     # initialize a dataframe
     out = {'sample_names': forestReader.sample_names_}
     for f in forestReader.feature_names_:
         out[f] = np.zeros((forestReader.number_of_samples_, ))
-    out = pd.dataframe(out, index = 'sample_names')
+    out = pd.DataFrame(out)
+    out = out.set_index('sample_names')
     # loop through each leaf, 
     #   for any sample it contains, add the feature importance
-    for ind in forestReader.info_.index: # TODO: make sure it is each row
+    for ind in forestReader.all_trees_.info_.index: # TODO: make sure it is each row
         for sample in out.index:
-            if forestReader.info_.loc[ind,sample]:
-                out.loc[sample,:] += forestReader.info_.loc[ind, forestReader.feature_names_]
+            if forestReader.all_trees_.info_.loc[ind,sample]:
+                out.loc[sample,:] += forestReader.all_trees_.info_.loc[ind, forestReader.feature_names_]
+    number_of_trees_ = len(np.unique(forestReader.all_trees_.info_['tree_id']))
     for sample in out.index:
-        out.loc[sample,:] = out.loc[sample,:] / number_of_trees
+        out.loc[sample,:] = out.loc[sample,:] / number_of_trees_
     return out
